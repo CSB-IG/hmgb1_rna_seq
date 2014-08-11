2c75e738f4ded2df254d8afb61ec484cb223f0ec
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-08-11 16:12:54 -0500

    assemble transcripts

---
+++ cufflinks.sh
@@ -0,0 +1,6 @@
+cufflinks-2.2.1.Linux_x86_64/cufflinks -p 24 -o Ctl-1 bams/Ctl-1.merged.bam
+cufflinks-2.2.1.Linux_x86_64/cufflinks -p 24 -o Ctl-4 bams/Ctl-4.merged.bam
+cufflinks-2.2.1.Linux_x86_64/cufflinks -p 24 -o Ctl-5 bams/Ctl-5.merged.bam
+cufflinks-2.2.1.Linux_x86_64/cufflinks -p 24 -o HMGB1-1 bams/HMGB1-1.merged.bam
+cufflinks-2.2.1.Linux_x86_64/cufflinks -p 24 -o HMGB1-4 bams/HMGB1-4.merged.bam
+cufflinks-2.2.1.Linux_x86_64/cufflinks -p 24 -o HMGB1-5 bams/HMGB1-5.merged.bam
