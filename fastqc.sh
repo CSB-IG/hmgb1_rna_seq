8a36b4e1633f03f67caae8bacf302c0cc0a1ce61
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-08-21 16:12:04 -0500

    raw read quality analyses

---
+++ fastqc.sh
@@ -0,0 +1,26 @@
+mkdir -p /media/e/gre_10036_rna/fastqc/Ctl-1_ATCACG_BC419JANXX_L006_001.R1
+mkdir -p /media/e/gre_10036_rna/fastqc/Ctl-1_ATCACG_BC419JANXX_L006_001.R2
+mkdir -p /media/e/gre_10036_rna/fastqc/Ctl-4_CGATGT_BC419JANXX_L006_001.R1
+mkdir -p /media/e/gre_10036_rna/fastqc/Ctl-4_CGATGT_BC419JANXX_L006_001.R2
+mkdir -p /media/e/gre_10036_rna/fastqc/Ctl-5_TTAGGC_BC419JANXX_L006_001.R1
+mkdir -p /media/e/gre_10036_rna/fastqc/Ctl-5_TTAGGC_BC419JANXX_L006_001.R2
+mkdir -p /media/e/gre_10036_rna/fastqc/HMGB1-1_TGACCA_BC419JANXX_L006_001.R1
+mkdir -p /media/e/gre_10036_rna/fastqc/HMGB1-1_TGACCA_BC419JANXX_L006_001.R2
+mkdir -p /media/e/gre_10036_rna/fastqc/HMGB1-4_ACAGTG_BC419JANXX_L006_001.R1
+mkdir -p /media/e/gre_10036_rna/fastqc/HMGB1-4_ACAGTG_BC419JANXX_L006_001.R2
+mkdir -p /media/e/gre_10036_rna/fastqc/HMGB1-5_GCCAAT_BC419JANXX_L006_001.R1
+mkdir -p /media/e/gre_10036_rna/fastqc/HMGB1-5_GCCAAT_BC419JANXX_L006_001.R2
+
+
+fastqc -o /media/e/gre_10036_rna/fastqc/Ctl-1_ATCACG_BC419JANXX_L006_001.R1 -t 24 /media/e/gre_10036_rna/fastq/Ctl-1_ATCACG_BC419JANXX_L006_001.R1.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/Ctl-1_ATCACG_BC419JANXX_L006_001.R2 -t 24 /media/e/gre_10036_rna/fastq/Ctl-1_ATCACG_BC419JANXX_L006_001.R2.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/Ctl-4_CGATGT_BC419JANXX_L006_001.R1 -t 24 /media/e/gre_10036_rna/fastq/Ctl-4_CGATGT_BC419JANXX_L006_001.R1.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/Ctl-4_CGATGT_BC419JANXX_L006_001.R2 -t 24 /media/e/gre_10036_rna/fastq/Ctl-4_CGATGT_BC419JANXX_L006_001.R2.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/Ctl-5_TTAGGC_BC419JANXX_L006_001.R1 -t 24 /media/e/gre_10036_rna/fastq/Ctl-5_TTAGGC_BC419JANXX_L006_001.R1.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/Ctl-5_TTAGGC_BC419JANXX_L006_001.R2 -t 24 /media/e/gre_10036_rna/fastq/Ctl-5_TTAGGC_BC419JANXX_L006_001.R2.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/HMGB1-1_TGACCA_BC419JANXX_L006_001.R1 -t 24 /media/e/gre_10036_rna/fastq/HMGB1-1_TGACCA_BC419JANXX_L006_001.R1.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/HMGB1-1_TGACCA_BC419JANXX_L006_001.R2 -t 24 /media/e/gre_10036_rna/fastq/HMGB1-1_TGACCA_BC419JANXX_L006_001.R2.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/HMGB1-4_ACAGTG_BC419JANXX_L006_001.R1 -t 24 /media/e/gre_10036_rna/fastq/HMGB1-4_ACAGTG_BC419JANXX_L006_001.R1.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/HMGB1-4_ACAGTG_BC419JANXX_L006_001.R2 -t 24 /media/e/gre_10036_rna/fastq/HMGB1-4_ACAGTG_BC419JANXX_L006_001.R2.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/HMGB1-5_GCCAAT_BC419JANXX_L006_001.R1 -t 24 /media/e/gre_10036_rna/fastq/HMGB1-5_GCCAAT_BC419JANXX_L006_001.R1.fastq.gz
+fastqc -o /media/e/gre_10036_rna/fastqc/HMGB1-5_GCCAAT_BC419JANXX_L006_001.R2 -t 24 /media/e/gre_10036_rna/fastq/HMGB1-5_GCCAAT_BC419JANXX_L006_001.R2.fastq.gz
