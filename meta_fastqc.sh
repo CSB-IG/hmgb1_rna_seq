4d75b5acef7d3ce042b1b1a50726529dd9ddccc6
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-08-25 16:46:42 -0500

    meta report on quality control of fastq files

---
+++ meta_fastqc.sh
@@ -0,0 +1,9 @@
+find . -iname 'duplication_levels.png' | sort >> meta_reporte.html
+find . -iname 'kmer_profiles.png' | sort >> meta_reporte.html
+find . -iname 'per_base_gc_content.png' | sort >> meta_reporte.html
+find . -iname 'per_base_n_content.png' | sort >> meta_reporte.html
+find . -iname 'per_base_quality.png' | sort >> meta_reporte.html
+find . -iname 'per_base_sequence_content.png' | sort >> meta_reporte.html
+find . -iname 'per_sequence_gc_content.png' | sort >> meta_reporte.html
+find . -iname 'per_sequence_quality.png' | sort >> meta_reporte.html
+find . -iname 'sequence_length_distribution.png' | sort >> meta_reporte.html
