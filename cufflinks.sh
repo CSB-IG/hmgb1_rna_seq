4d3cc466a5acd6287a699667e067a96693342d7d
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-09-17 20:11:38 -0500

    patched -G for --G

--- cufflinks.sh
+++ cufflinks.sh
@@ -4,8 +4,8 @@ GTF_ann=/media/e/gre_10036_rna/Human-gencode.v19/gencode.v19.annotation.gtf
 GTF_guide=/media/e/gre_10036_rna/Human-gencode.v19/gencode.v19.tRNAs.gtf
 
 $CUFFLINKS -g $GTF_guide -G $GTF_ann -p 24 -o Ctl-1 ${BAMS}/Ctl-1.merged.bam
-$CUFFLINKS -g $GTF_guide --G=$GTF_ann -p 24 -o Ctl-4 ${BAMS}/Ctl-4.merged.bam
-$CUFFLINKS -g $GTF_guide --G=$GTF_ann -p 24 -o Ctl-5 ${BAMS}/Ctl-5.merged.bam
-$CUFFLINKS -g $GTF_guide --G=$GTF_ann -p 24 -o HMGB1-1 ${BAMS}/HMGB1-1.merged.bam
-$CUFFLINKS -g $GTF_guide --G=$GTF_ann -p 24 -o HMGB1-4 ${BAMS}/HMGB1-4.merged.bam
-$CUFFLINKS -g $GTF_guide --G=$GTF_ann -p 24 -o HMGB1-5 ${BAMS}/HMGB1-5.merged.bam
+$CUFFLINKS -g $GTF_guide -G $GTF_ann -p 24 -o Ctl-4 ${BAMS}/Ctl-4.merged.bam
+$CUFFLINKS -g $GTF_guide -G $GTF_ann -p 24 -o Ctl-5 ${BAMS}/Ctl-5.merged.bam
+$CUFFLINKS -g $GTF_guide -G $GTF_ann -p 24 -o HMGB1-1 ${BAMS}/HMGB1-1.merged.bam
+$CUFFLINKS -g $GTF_guide -G $GTF_ann -p 24 -o HMGB1-4 ${BAMS}/HMGB1-4.merged.bam
+$CUFFLINKS -g $GTF_guide -G $GTF_ann -p 24 -o HMGB1-5 ${BAMS}/HMGB1-5.merged.bam
