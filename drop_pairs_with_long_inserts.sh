196526572f9e896a24fd7b0f4b41075fb6a069d5
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-10-14 12:24:19 -0500

    mass process of 2500i filtered files

---
+++ drop_pairs_with_long_inserts.sh
@@ -0,0 +1,7 @@
+python drop_pairs_with_long_inserts.py --mean 150 --s 2500 --bam_in ../bams/Ctl-1.merged.bam --bam_out ../bams/Ctl-1.paired_isized_2500.sam &
+python drop_pairs_with_long_inserts.py --mean 150 --s 2500 --bam_in ../bams/Ctl-4.merged.bam --bam_out ../bams/Ctl-4.paired_isized_2500.sam &
+python drop_pairs_with_long_inserts.py --mean 150 --s 2500 --bam_in ../bams/Ctl-5.merged.bam --bam_out ../bams/Ctl-5.paired_isized_2500.sam &
+python drop_pairs_with_long_inserts.py --mean 150 --s 2500 --bam_in ../bams/HMGB1-1.merged.bam --bam_out ../bams/HMGB1-1.paired_isized_2500.sam &
+python drop_pairs_with_long_inserts.py --mean 150 --s 2500 --bam_in ../bams/HMGB1-4.merged.bam --bam_out ../bams/HMGB1-4.paired_isized_2500.sam &
+python drop_pairs_with_long_inserts.py --mean 150 --s 2500 --bam_in ../bams/HMGB1-5.merged.bam --bam_out ../bams/HMGB1-5.paired_isized_2500.sam &
+
--- sam_sort.sh
+++ sam_sort.sh
@@ -1,6 +1,6 @@
-samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/Ctl-1.merged.bam Ctl-1.sorted &
-samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/Ctl-4.merged.bam Ctl-4.sorted &
-samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/Ctl-5.merged.bam Ctl-5.sorted &
-samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/HMGB1-1.merged.bam HMGB1-1.sorted &
-samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/HMGB1-4.merged.bam HMGB1-4.sorted &
-samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/HMGB1-5.merged.bam HMGB1-5.sorted &
+samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/Ctl-1.paired_isized_2500.bam Ctl-1.2500.sorted &
+samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/Ctl-4.paired_isized_2500.bam Ctl-4.2500.sorted &
+samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/Ctl-5.paired_isized_2500.bam Ctl-5.2500.sorted &
+samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/HMGB1-1.paired_isized_2500.bam HMGB1-1.2500.sorted &
+samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/HMGB1-4.paired_isized_2500.bam HMGB1-4.2500.sorted &
+samtools sort -n -m 10G -@ 8 /media/e/gre_10036_rna/bams/HMGB1-5.paired_isized_2500.bam HMGB1-5.2500.sorted &
